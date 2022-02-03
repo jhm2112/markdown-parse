f00f56d3c6eb7b88e1e4e849b8b2619607e764f9
jhm2112 <79220026+jhm2112@users.noreply.github.com>
2022-02-03 14:30:16 -0800

    new test case

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -44,5 +44,17 @@ public class MarkdownParseTest {
             ArrayList<String> links = MarkdownParse.getLinks(contents);
             assertEquals(0, links.size());
         }
+
+        @Test
+        public void getLinks5() throws IOException {
+            Path fileName = Path.of("breaking-test_4.md");
+            String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals(0, links.size());
+        }
+
+
+
+
 }
 
