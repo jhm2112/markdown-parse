0419c7b124c84b1f7129ab5c136ef926672f148b
jhm2112 <79220026+jhm2112@users.noreply.github.com>
2022-02-25 14:01:20 -0800

    lab8 updates

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -53,8 +53,36 @@ public class MarkdownParseTest {
             assertEquals(0, links.size());
         }
 
+        @Test
+        public void testSnippet1() throws IOException {
+            Path fileName = Path.of("snippet1.md");
+            String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals("`google.com", links.get(0));
+            assertEquals("google.com", links.get(1));
+            assertEquals("ucsd.edu", links.get(2));
+            assertEquals(3, links.size());
+        }
 
+        @Test
+        public void testSnippet2() throws IOException {
+            Path fileName = Path.of("snippet2.md");
+            String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals("a.com", links.get(0));
+            assertEquals("a.com(())", links.get(1));
+            assertEquals("example.com", links.get(2));
+             assertEquals(3, links.size());
+        }
 
+        @Test
+        public void testSnippet3() throws IOException {
+            Path fileName = Path.of("snippet3.md");
+            String contents = Files.readString(fileName);
+            ArrayList<String> links = MarkdownParse.getLinks(contents);
+            assertEquals(" https://ucsd-cse15l-w22.github.io/", links.get(0));
+             assertEquals(1, links.size());
+        }
 
 }
 
